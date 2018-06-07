23487f9ed862e258bc916ea8718caec91863a304
George Wang <george.siqi.wang@gmail.com>
2018-06-07 11:15:56 -0400

    Update util.php

--- util.php
+++ util.php
@@ -5,7 +5,6 @@ function configSession() {
   if (!isset($db)) {
     define('DB_SERVER', '127.0.0.1');
     define('DB_USERNAME', 'root');
-    // define('DB_PASSWORD', 'Shgl123.');
     define('DB_PASSWORD', '');
     define('DB_DATABASE', 'alfar_db1');
     global $db;
